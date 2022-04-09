ad8de1a320c2e21243dcabe4fbc19420e33c1adf
Ast415 <bastienbories81@gmail.com>
2022-04-09 14:09:39 +0200

    Update dépôt_formulaire et création function.js
    L'input type=date sera par defaut mis a la date du jour.
    Et création du dossier JavaScript et du fichier function.js qui contiendra tout les fonction javascript.

---
+++ JavaScript/function.js
@@ -0,0 +1,14 @@
+function DateDuJour(){
+    RecuperationDateSys = new Date();
+    annee = RecuperationDateSys.getFullYear();
+    mois = RecuperationDateSys.getMonth() + 1;
+    if (mois<10){
+        mois="0"+mois;
+    }
+    jour = RecuperationDateSys.getDate();
+    if (jour<10){
+        jour="0"+jour;
+    }
+    DateBonFormat=(annee+"-"+mois+"-"+jour);
+    return DateBonFormat;
+}
