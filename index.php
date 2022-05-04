6f8ae1a2c0704521c014e1658a08decb83462fb5
Ast415 <bastienbories81@gmail.com>
2022-05-04 18:29:43 +0200

    Update index.php
    Ajout du link CSS vers Banniere.css. Avant elle de fessai dans le superflus des baniere

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GLPI/login</title>
     <link href="CSS/MaquetteCSS.css" rel="stylesheet">
+    <link href="CSS/Banniere.css" rel="stylesheet">
     <script type="text/javascript">
         function verifChamps()
         {
