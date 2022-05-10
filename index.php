b9f0293d48e71c7588e33bc302517c7e15c6cb6d
killian24000 <102808884+killian24000@users.noreply.github.com>
2022-05-10 11:17:03 +0200

    update

--- index.php
+++ index.php
@@ -36,7 +36,6 @@
         <button type="reset" name="annulation" value="Annuler">Annuler</button>
         <br>
         <br>
-        coucou
         <p> 
           Vous êtes un nouvel utilisateur vous devez remplir le <a href="formulaire inscription.html">formulaire d'inscription</a>      
         </p>
