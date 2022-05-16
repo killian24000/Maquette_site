4756420bf6799b5ff172244c5d099fec7b70a95d
killian24000 <102808884+killian24000@users.noreply.github.com>
2022-05-16 13:52:12 +0200

    update

--- Page_PRACTICIEN.php
+++ Page_PRACTICIEN.php
@@ -2,14 +2,21 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link href="CSS/MaquetteCSS.css" rel="stylesheet">
+            <link href="CSS/Banniere.CSS" rel="stylesheet">
         <title>GLPI/Practicien</title>
     </head>
     <body>
     <?php
     include("HautDePage.HTML")
     ?>
-     <h1>Praticien</h1>
+    <h1>Praticien</h1>
+    <form name="ListeRecherche">
+        <select name="LstPraticien" class="titre">
+            <option>Choisissez un praticien</option>
 
-
+    <?php
+    include("BasDePage.HTML")
+    ?>
     </body>
 </html>
