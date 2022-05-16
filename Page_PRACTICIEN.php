fbe123dfadf7bdad1a78463baa0dc4ddc08d8db0
killian24000 <102808884+killian24000@users.noreply.github.com>
2022-05-16 17:54:00 +0200

    update

--- Page_PRACTICIEN.php
+++ Page_PRACTICIEN.php
@@ -10,6 +10,10 @@
     <?php
     include("HautDePage.HTML")
     ?>
+    <?php
+    //Appel du script de connexion 
+    require('connect.php');
+    ?>
     <div id="titre">
     <h1>Praticien</h1>
     <form name="ListeRecherche">
@@ -20,6 +24,37 @@
             <option value="3">Delacoure</option>
         </select>    
     </form>
+    <?php
+    $reqSQL="SELECT mednom FROM medecin"
+    $result=$connexion->query($reqSQL);
+    $ligne=$result->fetch();
+    while($ligne!=false)
+    {
+       $ligne = $result->fetch(); 
+    }
+    ?>
+    <select name="Valider">
+        <?php
+        $reqSQL="SELECT mednom FROM classe";
+        //Exécute la requête
+        $result=$connexion->query($reqSQL);
+        //Lecture de la 1re ligne du jeu d'enregistrements
+        $ligne=$result->fetch();
+        //Tant qu'on n'a pas atteint la fin du jeu d'enregistrements,on boucle
+        while($ligne!=false)
+        {   //On stock les données de la classe dans des variables
+            $code=$ligne["MedCode"]; //code du praticien
+            $nom=$ligne["MedNom"];  //Nom du praticien
+            $prenom=$ligne["MedPrenom"]; //Prenom du praticien
+            $adresse=$ligne["MedAdresse"]; //adresse du praticien
+            $ville=$ligne["MedVille"]; //ville du praticien
+            $CP=$ligne["MedCP"]; //Code postal du praticien
+            //On génère une ligne dans la liste déroulante 
+            echo"<option value='$_POST'>$_POST</option>";
+            $ligne=$result->fetch();
+        }
+        ?>
+    </select>
     <br>
     <button type="submit" name="Valider" value="Valider">Rechercher</button>
     <ul>
