55f12163758d73c020886821ec8c8b794196e450
killian24000 <102808884+killian24000@users.noreply.github.com>
2022-05-16 14:32:38 +0200

    Update

--- Page_PRACTICIEN.php
+++ Page_PRACTICIEN.php
@@ -10,12 +10,19 @@
     <?php
     include("HautDePage.HTML")
     ?>
-    <div id="Titre">
+    <div id="titre">
     <h1>Praticien</h1>
     <form name="ListeRecherche">
         <select name="LstPraticien" class="titre">
             <option>Choisissez un praticien</option>
+            <option value="1">Dupont</option>
+            <option value="2">Gosselin</option>
+            <option value="3">Delacoure</option>
+        </select>    
     </form>
+    <br>
+    <button type="submit" name="Valider" value="Valider">Rechercher</button>
+    <ul>
     </div>
     <?php
     include("BasDePage.HTML")
