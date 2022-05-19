df64021f9ba9b658324abe8c355a961693d33842
killian24000 <102808884+killian24000@users.noreply.github.com>
2022-05-19 15:33:21 +0200

    update

--- FAQ.php
+++ FAQ.php
@@ -10,14 +10,22 @@
         <?php
         include("HautDePage.HTML")
         ?>
-        <div id="question">
+        <div id="FAQ">
+            <div class="question" id="Q1">
             <h2>Je n'arrive pas à me connecter que dois-je faire ? </h2>
-            <h3>Pour cela il suffit de contacter le service réseau</h3> 
+            </div>
+            <h3>Pour cela il suffit de contacter le service réseau</h3>
+            <div class="question"> 
             <h2>J'ai perdu mes identifiants, qui dois-je contacter ?</h2>
-            <h3>En cas de perte de vos identifiants, contacter le service informatique qui vous en attribuera des nouveaux  </h3>        
+            </div>
+            <h3>En cas de perte de vos identifiants, contacter le service informatique qui vous en attribuera des nouveaux  </h3>
+            <div class="question">        
             <h2>Que doit-je faire en cas de piratage du compte ?</h2>
+            </div>
             <h3>En cas de piratage de votre compte nous vous recommandons de changer votre mot de passe</h3>
+            <div class="question">
             <h2>Je suis un nouveau dans l'entreprise, comment obtenir mes identifiants ?</h2>
+            </div>
             <h3>Veuillez vous rapprochez du service informatique de l'entreprise </h3> 
         </div>
         <?php 
--- Page_MEDICAMENT.php
+++ Page_MEDICAMENT.php
@@ -5,63 +5,89 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="CSS/MaquetteCSS.css" rel="stylesheet">
-        <link href="CSS/Banniere.CSS" rel="stylesheet">
+    <link href="CSS/Banniere.CSS" rel="stylesheet">
+    <link href="CSS/Formulaire.css" rel="stylesheet">
     <title>GSB/Médicament</title>
 </head>
     <body>
-        <?php
-        include("HautDePage.HTML")
-        ?>
-        <div style="display: flex" id="CorpDePage">
-            <?php
-            include('Raccourci.html')
-            ?>
-            <div>
-            <div id="Test">
-                    <p>
-                        <h2>PHARMACOPEE</h2> 
-                    </p>
+    <?php
+    include("HautDePage.HTML")
+    ?>
+    <div style="display: flex" id="CorpDePage">
+    <?php
+    include('Raccourci.html')
+    ?>
+   
+    <?php
+    //Appel du script de connexion
+    include('connect.php');
+    //---------------------------------------------------------------------
+	// R�cup�rer les valeurs saisies dans le formulaire dans les variables
+	//---------------------------------------------------------------------
+    function test(){
+        $DepotLegal=$_POST["DepotLegal"];
+        $NomCommercial=$_POST["NomCommercial"];
+        $Famille=$_POST["Famille"];
+        $Composition=$_POST["Composition"];
+        $Effets=$_POST["Effets"];
+        $ContraIndication=$_POST["ContreIndication"];
+        $Prix=$_POST["Prix"];
+        echo("$DepotLegal"." "."$NomCommercial"." "."$Famille".""."$Composition".""."$Effets".""."$ContreIndication");
+        //On récupère dans des variables les données saisies par l'utilisateur
+        $reqSQL="INSERT INTO medicament VALUES ('$DepotLegal','$NomCommercial','$Famille','$Composition','$Effets','$ContraIndication','$Prix')";
+        
+        //Execution de la requête
+        $connexion->exec($reqSQL) or die ("erreur dans la requête sql");
+    
+        //on ferme la connexion
+        $connexion=null;
+
+    }
+    ?>
+    <form action="Page_MEDICAMENT.php" method="POST" onsubmit="test()">
+    <div id="CDPCentre">
+        <div id="Test245">
+            <p>
+            <h2>PHARMACOPEE</h2> 
+            </p>
                 </div>
                 <div id="medicament_corps">
-                    <br>
-                        DEPOT LEGAL : 
-                            <p>
-                                <input type="text">
-                            </p>
-                        NOM COMMERCIAL : 
-                            <p>
-                                <input type="text">
-                            </p>
-                        FAMILLE :
-                            <p>
-                                <input type="text">
-                            </p>
-                        COMPOSANT : 
-                            <p>
-                                <textarea name="" id="" cols="30" rows="10" maxlength="1000"></textarea>
-                            </p>
-                        EFFETS :
-                            <p>
-                                <textarea name="" id="" cols="30" rows="10" maxlength="1000"></textarea>
-                            </p>
-                        CONTRE INDICATION :
-                            <p>
-                                <textarea name="" id="" cols="30" rows="10" maxlength="1000"></textarea>
-                            </p>
-                        PRIX ECHANTILLON : 
-                            <p>
-                                <textarea name="" id="" cols="30" rows="10" maxlength="1000"></textarea>
-                            </p>                        
-                </div>
+                <br>
+                <p>
+                DEPOT LEGAL :  <input type="text" name='$DepotLegal'>
+                </p>
+                <p>
+                NOM COMMERCIAL : <input type="text" name='$NomCommercial'>
+                </p>
+                <p>
+                FAMILLE: <input type="text" name='$Famille'>
+                </p>
+                COMPOSANT : 
+                <p>
+                <textarea name="" id="" cols="30" rows="10" maxlength="1000" name='$Composition'></textarea>
+                </p>
+                EFFETS :
+                <p>
+                <textarea name="" id="" cols="30" rows="10" maxlength="1000" name='$Effets'></textarea>
+                </p>
+                CONTRE INDICATION :
+                <p>
+                <textarea name="" id="" cols="30" rows="10" maxlength="1000" name='$ContraIndication'></textarea>
+                </p>
+                PRIX ECHANTILLON : 
+                <p>
+                <textarea name="" id="" cols="30" rows="10" maxlength="1000" name='$Prix'></textarea>
+                </p>
+                <button type="submit">Ajouter</button>            
             </div>
-            </div>
-            <div id="ZoneRaccourci"></div>
         </div>
-            
-            
+        <div id="ZoneRaccourci"></div>
         </div>
-        <?php
-        include("BasDePage.HTML")
-        ?>
+    </form>
+    </div>     
+    </div>
+    <?php
+    include("BasDePage.HTML")
+    ?>
     </body>
 </html>
